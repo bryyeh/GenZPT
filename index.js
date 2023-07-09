e57f32b9f2999f7ad79a0f93ec6b5d6451758d47
Krishan Gupta <bkgupta@gmail.com>
2023-07-09 07:07:59 -0700

    working on twilio

--- index.js
+++ index.js
@@ -30,16 +30,59 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Twilio
+const twilio = require('twilio');
+const VoiceResponse = twilio.twiml.VoiceResponse;
+
+if(!process.env.TWILIO_ACCOUNT_SID || !process.env.TWILIO_AUTH_TOKEN) {
+    console.log("Please set the TWILIO_ACCOUNT_SID and TWILIO_AUTH_TOKEN environment variables in your .env file. \nYou can find your Twilio credentials at https://www.twilio.com/console")
+    process.exit(1)
+}
+
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID // 'your_account_sid';
+const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN // 'your_auth_token';
+
+
 
 /******************************** Routes ********************************/
 
 
 app.get('/', (req, res) => {
     res.render("call_form")
-//   res.send('Hello World!')
 })
 
 app.post('/call', (req, res) => {
+
+    const toPhoneNumber = req.body.phoneNumber
+    const purpose = req.body.purpose
+    const fromPhoneNumber = '555 555 5555'
+
+    const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    const twiml = new VoiceResponse()
+    twiml.say(`Calling ${toPhoneNumber} with the purpose of ${purpose}`)
+  
+    const callOptions = {
+      to: toPhoneNumber,
+      from: fromPhoneNumber,
+      twiml: twiml.toString()
+    }
+  
+    client.calls.create(callOptions)
+      .then(call => {
+        const callSid = call.sid
+        const audioUrl = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Calls/${callSid}/Recordings.json`
+  
+        res.render('call', {
+          audioUrl: audioUrl,
+          phoneNumber: req.body.phoneNumber,
+          purpose: req.body.purpose
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).send('Error making call');
+      });    
 
     res.render("call", {
         phoneNumber: req.body.phoneNumber, 
